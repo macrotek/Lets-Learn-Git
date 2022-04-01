b971e8b17b3e1efc34b5d72b16df3909132317c1
Ganesh Babar <ganuda1234@gmail.com>
2022-04-01 10:43:22 +0530

    committed n1.c and n1.exe

---
+++ n1.c
@@ -0,0 +1,16 @@
+// The sum of two numbers
+#include<stdio.h>
+int main()
+{
+	//int num1, num2, sum;
+	Dosum();
+	return 0;
+}
+Dosum()
+{
+	int num1, num2, sum;
+	printf("\Enter the two numbers:");
+	scanf("%d %d",&num1,&num2);
+	sum=num1+num2;
+	printf("The sum of two numbers is :%d",sum);
+}
